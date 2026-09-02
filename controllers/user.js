a8c8f537128fa0ff8568fe2299ce539569b7a31a
Tuhin <karmakartuhin611@gmail.com>
2026-09-03 00:57:51 +0530

    Add automatic login after signup

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,7 @@ const {setUser} = require("../service/auth");
 const handleUserSignup = async (req, res) => {
     const { name, username, email, password } = req.body;
 
-    await User.create({
+    const user = await User.create({
         name,
         username,
         email,
