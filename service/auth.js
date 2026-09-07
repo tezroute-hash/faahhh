ac2f61481e6da21f8badbd49e66d9f9f65d05cf9
Tuhin <karmakartuhin611@gmail.com>
2026-09-07 08:08:32 +0530

    updated authentication

--- service/auth.js
+++ service/auth.js
@@ -1,17 +1,16 @@
 const jwt = require("jsonwebtoken");
-const secret = "Tuhin@#$2004";
 
 function setUser(user) {
     return jwt.sign({
         _id : user._id,
         email : user.email,
-    }, secret);
+    }, process.env.JWT_SECRET);
 }
 
 function getUser(token) {
     if(!token) return null;
     try{
-        return jwt.verify(token, secret);
+        return jwt.verify(token, process.env.JWT_SECRET);
     }catch(err){
         return null;
     }
